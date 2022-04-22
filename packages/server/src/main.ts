6171e8fe8d82849691cc3556932eaaf5f3fe8fca
Jon Lervold <jonlervold@gmail.com>
2022-04-22 11:33:01 -0600

    Pulled in endpoint from legacy repo

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -4,12 +4,46 @@
  */
 
 import * as express from 'express';
+import * as cors from "cors";
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to server!' });
+let database = {
+  experiencedthings: {
+    fileName: "experiencedthings",
+    extension: "jpg",
+    description: "text saying you have just experienced things",
+    dateAdded: "04/11/2022",
+  },
+  literalspongebob: {
+    fileName: "literalspongebob",
+    extension: "jpg",
+    description: "really bad spongebob costume made of actual sponges",
+    dateAdded: "04/11/2022",
+  },
+};
+
+app.use(cors());
+
+app.use(express.static("./packages/server/public"));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("server is active");
 });
+
+app.get("/database", (req, res) => {
+  res.send(database);
+});
+
+app.put("/database", (req, res) => {
+  res.send("Received PUT request in /database");
+  // needs to actually update the database here... HOW?
+  console.log(req.body); // ??
+  database = req.body;
+});
+
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {

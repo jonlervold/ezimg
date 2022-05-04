90c474220865aef9d955e1a867bd52c2a97f50d5
Jon Lervold <jonlervold@gmail.com>
2022-05-03 19:53:19 -0600

    Add color to RenameFile error

--- packages/app/src/components/RenameFile.tsx
+++ packages/app/src/components/RenameFile.tsx
@@ -3,12 +3,19 @@ import { getSystemErrorMap } from 'util';
 import fetchDatabase from '../api/fetchDatabase';
 import renameFile from '../api/renameFile';
 import useRenameFile from '../hooks/useRenameFile';
+import styled from 'styled-components';
 
 type Props = {
   filename: string;
   extension: string;
   setChange: React.Dispatch<SetStateAction<string>>;
 };
+
+const Container = styled.div`
+  .error {
+    color: red;
+  }
+`;
 
 const RenameFile: FC<Props> = ({ filename, extension, setChange }) => {
   const { value, setValue, error, onClick } = useRenameFile(
@@ -19,12 +26,14 @@ const RenameFile: FC<Props> = ({ filename, extension, setChange }) => {
 
   return (
     <td>
-      <input
-        onChange={(e) => setValue(e.target.value)}
-        value={value.toLowerCase().replace(/[^a-z\d-]/g, '')}
-      ></input>{' '}
-      <button onClick={onClick}>Rename</button>
-      <div>{error}</div>
+      <Container>
+        <input
+          onChange={(e) => setValue(e.target.value)}
+          value={value.toLowerCase().replace(/[^a-z\d-]/g, '')}
+        ></input>{' '}
+        <button onClick={onClick}>Rename</button>
+        <div className="error">{error}</div>
+      </Container>
     </td>
   );
 };
